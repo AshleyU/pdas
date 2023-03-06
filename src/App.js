f25c46e625076288fa1bc7cb02e495a1ae59438f
Ashley U <ashley.umetsu@gmail.com>
2023-03-05 22:55:24 -0600

    adding cards and title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title has-text-centered'>Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns is-centered'>
+                        <div className='column is-3'>
+                            <ProfileCard title="Alexa" handle="@alexa99" img={AlexaImage} altText="Alexa logo" description="Alexa is Amazon's cloud-based voice service." />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard title="Cortana" handle="@cortana32" img={CortanaImage} altText="Cortana logo" description="Cortana is Microsoft's personal productivity assistant."/>
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard title="Siri" handle="@siri01" img={SiriImage} altText="Siri logo" description="Siri is a voice recognition application created by Apple." />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,22 @@
-function ProfileCard() {
-    return <div>Profile Card</div>;
+function ProfileCard({ title, handle, img, altText, description }) {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={img} alt={altText} />
+                </figure>    
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <br></br>
+                <div className="content">{description}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ProfileCard;
